7a28622956cca808278bc0324135b19fbdd154a7
Lucas Haubmann <lucas.a.haubmann@gmail.com>
2026-01-18 21:27:36 -0300

    #1 feat: Creating the PayloadController class.

--- src/main/java/com/poc/webhook/WebhookApplication.java
+++ src/main/java/com/poc/webhook/WebhookApplication.java
@@ -2,7 +2,9 @@ package com.poc.webhook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 @SpringBootApplication
 public class WebhookApplication {
 
